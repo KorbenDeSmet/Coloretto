623def09649f0cdf840450a2a46eb2b1a6d20082
KorbenDeSmet <Korbendesmet@hotmail.com>
2020-03-09 12:25:21 +0100

    Gewerkt aan klasse Kaart

--- src/domein/Kaart.java
+++ src/domein/Kaart.java
@@ -1,5 +1,34 @@
 package domein;
 
-public class Kaart {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Kaart {
+	private String kleur;
+	private static final List<String> KLEURENLIJST = new ArrayList<>(
+			Arrays.asList("Rood", "Paars", "Blauw", "Groen", "Bruin", "Oranje", "Geel", "+2", "Joker")
+	);
+
+	
+	public Kaart(String kleur) {
+		this.kleur = kleur;
+	}
+
+	public String getKleur() {
+		return kleur;
+	}
+
+	public void setKleur(String kleur) {
+		if (KLEURENLIJST.contains(kleur))
+			this.kleur = kleur;
+		else
+			throw new IllegalArgumentException("Foute invoer!");
+	}
+	
+	
+	@Override
+	public String toString() {
+			return "Kaart in de kleur " + this.kleur;
+	}
 }
