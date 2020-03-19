f42c00bb159b5b034c92238940999ff4714a9cfe
KorbenDeSmet <Korbendesmet@hotmail.com>
2020-03-19 14:45:57 +0100

    Schrijven DomeinController.java

--- src/domein/DomeinController.java
+++ src/domein/DomeinController.java
@@ -1,5 +1,48 @@
 package domein;
 
+import java.util.List;
+
 public class DomeinController {
+	private Spel spel;
 	
+	public void startSpel() {
+		this.spel = new Spel();
+	}
+	
+	/* wordt automatisch uitgevoerd in de constructor van klasse Spel
+	public void deckAanmaken() {
+		spel.deckAanmaken();
+	}*/
+	
+	public void spelersToevoegen(List<String> namenSpelers) {
+		spel.spelersToevoegen(namenSpelers);
+	}
+	
+	public void spelersStartInventoryGeven() {
+		spel.spelersStartInventoryGeven();
+	}
+	
+	public String neemKaart() {
+		return spel.neemKaart();
+	}
+	
+	public void stapelsAanmaken() {
+		spel.stapelsAanmaken();
+	}
+	
+	public List<Stapel> stapelsTonen() {
+		return spel.stapelsTonen();
+	}
+	
+	public void plaatsKaartOpStapel(int stapelNR, Kaart inHand) {
+		spel.plaatsKaartOpStapel(stapelNR, inHand);
+	}
+	
+	public void stapelNemen(int nummerVanSpeler, int stapelNR) {
+		spel.stapelNemen(nummerVanSpeler, stapelNR);
+	}
+	
+	public String scoreBerekenen(Speler speler) {
+		return spel.scoreBerekenen(speler);
+	}
 }
