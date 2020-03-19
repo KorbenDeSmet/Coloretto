f2a230609b5a3a5a2160500d83f152f626e49c45
KorbenDeSmet <Korbendesmet@hotmail.com>
2020-03-19 15:11:19 +0100

    Update ColorettoApplicatie.java

--- src/ui/ColorettoApplicatie.java
+++ src/ui/ColorettoApplicatie.java
@@ -1,10 +1,33 @@
 package ui;
 
+import java.util.Scanner;
+
+import domein.DomeinController;
+
 public class ColorettoApplicatie {
 
+	private static Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// sysout + naamArray werkt perfect!
-
+		// sysout + naamArray werkt perfect	
+		DomeinController domeinController = new DomeinController();
+		int aantalSpelers;
+		domeinController.startSpel();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		scanner.close();
 	}
-
 }
