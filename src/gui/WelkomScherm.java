9e0c10fb73fa1e49d5f6cf7590794ec06b5a7ed8
falkodhondt <dondt.oklaf@outlook.com>
2020-04-28 16:03:52 +0200

    prutsen menu/toolbar
    door dat ik de imports hier niet in orde krijg krijg ik fout op elk keyworddat er uitkomt vandaar zoveel fouten

--- src/gui/WelkomScherm.java
+++ src/gui/WelkomScherm.java
@@ -1,28 +1,51 @@
 package gui;
+
+
+import javafx.application.Platform;
+import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-//import domein.DomeinController;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import domein.DomeinController;
 
 public class WelkomScherm extends Pane {
 	//DomeinController domeinController = new DomeinController();
 	public WelkomScherm () {
 		
-		Label lblwelkom = new Label("welko; bij javafx");
+		Label lblwelkom = new Label("welkom bij javafx");
 		
+		//	menubar maken 
+		
+		 MenuBar menubar = new MenuBar ();
+		 Menu filemenu = new Menu("menu");
+		 MenuItem infoMenuItem = new MenuItem("info");
+		 MenuItem highscoreMenuItem = new MenuItem("highscore");
+		 MenuItem exitMenuItem = new MenuItem("exit");
+		 aboutMenuItem.setOnAction(this::aboutClicked);
+		 exitMenuItem.setOnAction( new EventHandler<ActionEvent>() {
+			 @Override
+			 public void handle(ActionEvent e) {
+				 Platform.exit();
+			 } 	 
+		 } ) ;
+				
+			//toolbar
+		 	
+		 Button btnRed = new Button("with");
+		 Button btnGreen = new Button("green"); 
+		 Button btnBlue = new Button("blue"); 
+		 Button btnBlack = new Button("black");
+		 ToolBar tooolBar = new ToolBar( new Label("background color")
+				 				,btnRed,btnGreen,btnBlue,btnBlack);
+		 toolBar.setOrientation(Orientation.HORIZONTAL);
+				 
+		 
 	
 		
-		
-		
-		
-	}
-	
-	
-	
-	
-	
-	
-	
+	}	
 	
 }
