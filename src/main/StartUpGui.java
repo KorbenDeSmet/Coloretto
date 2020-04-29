5aaaaf8bb33d6e2a3ba67f0abbd3f7c8abaa1d50
KorbenDeSmet <Korbendesmet@hotmail.com>
2020-04-29 17:34:04 +0200

    Opkuis

--- src/main/StartUpGui.java
+++ src/main/StartUpGui.java
@@ -1,6 +1,5 @@
-/*package main;
+package main;
 
-import domein.Spel;
 import gui.WelkomScherm;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -21,30 +20,3 @@ public class StartUpGui extends Application{
         launch(args);
     }
 }
-*/
-
-package main;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import gui.WelkomScherm;
-
-public class StartUpGui extends Application {
-	@Override
-	public void start(Stage Stage) {
-		WelkomScherm welkom = new WelkomScherm();
-		Scene scene = new Scene(welkom, 500,300);
-		//scene.getStylesheets().add(getClass().getResource("/css/main.css").toExternalForm());
-		Stage.setScene(scene);
-		Stage.setMaximized(true);
-		Stage.setTitle("Coloretto");
-		Stage.show();
-	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
-
-}
-*/
