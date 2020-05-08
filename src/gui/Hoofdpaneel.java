4b031a1e0f0d04da36cd84cb5962743b85800a37
KorbenDeSmet <Korbendesmet@hotmail.com>
2020-05-08 18:02:33 +0200

    Update Hoofdpaneel.java

--- src/gui/Hoofdpaneel.java
+++ src/gui/Hoofdpaneel.java
@@ -4,7 +4,9 @@ package gui;
 import domein.DomeinController;
 import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class Hoofdpaneel extends GridPane {
@@ -13,19 +15,26 @@ public class Hoofdpaneel extends GridPane {
 	private WelkomScherm ws = new WelkomScherm(this);
 	private Spel spel = new Spel(this);
 	
+	private Label lblWelkom;
 	private Button btnStart;
 	
 	public Hoofdpaneel() {
+		domeinController.startSpel();
 		super.setPadding(new Insets(10, 10, 10, 10));
 		super.setVgap(5);
 		super.setHgap(5);	
+		super.setAlignment(Pos.CENTER); 
 		
-		btnStart = new Button("Start spel!");
-		super.add(btnStart, 1, 1);
+		lblWelkom = new Label("Welkom bij Coloretto!");
+		super.add(lblWelkom, 1, 1);
+		
+		btnStart = new Button("Nieuw spel starten!");
+		super.add(btnStart, 1, 2);
 		btnStart.setOnAction(this::toonWelkomScherm);
 	}	
 	
 	public void toonWelkomScherm(ActionEvent event) {
+		getChildren().remove(lblWelkom);
 		getChildren().remove(btnStart);
 		getChildren().add(ws);
 	}
